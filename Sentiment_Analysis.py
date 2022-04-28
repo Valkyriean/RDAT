0c3bd1f4b995a9ee066b6c581f6bf362c855a7fa
Jiachen Li <alexjiachenli@hotmail.com>
2022-04-28 17:49:02 +1000

    add emoji handling in sentiment analysis

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -1,10 +1,21 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
 nltk.download('vader_lexicon')
+
+from emot.emo_unicode import UNICODE_EMOJI
+
 sia = SentimentIntensityAnalyzer()
 text = '''
-Not anymore and hopefully not again #lockdown #covid #corona #fitzroy #graffiti #slogangraffiti @graffiterati @sevenbreaths
+Hilarious 😂. The feeling of making a sale 😎, The feeling of actually fulfilling orders 😒
 '''
+def convert_emojis(text):
+    for emot in UNICODE_EMOJI:
+        text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",","").replace(":","").split()))
+    return text.replace("_"," ")
+
+print(text)
+text1 = convert_emojis(text)
+print(text1)
 
 res = sia.polarity_scores(text)
 
