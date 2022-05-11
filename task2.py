97dacb7317a5cb1cec322cae3f20f027d791b9e8
Jiachen Li <alexjiachenli@hotmail.com>
2022-05-11 13:27:49 +1000

    fix time slot for trend and add mini gun style crawler

--- task2.py
+++ task2.py
@@ -16,45 +16,46 @@ from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
 import keras
 
-
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
+
 stopwords = nltk.corpus.stopwords.words('english')
 
 file = "covid.data.txt"
 
 
-BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAIHFbAEAAAAA0oBVG5orLLErnyAqw2po3fOae5w%3D4lgZWoMOyGG496F2aNACoKOdDCaDnxqret6oFPLToE244O6Tx6"
+# BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAIHFbAEAAAAA0oBVG5orLLErnyAqw2po3fOae5w%3D4lgZWoMOyGG496F2aNACoKOdDCaDnxqret6oFPLToE244O6Tx6"
 
-
-
-
+tokens = ["AAAAAAAAAAAAAAAAAAAAAIHFbAEAAAAA0oBVG5orLLErnyAqw2po3fOae5w%3D4lgZWoMOyGG496F2aNACoKOdDCaDnxqret6oFPLToE244O6Tx6",
+          "AAAAAAAAAAAAAAAAAAAAABoebgEAAAAA8nOwpOH6GviG0pVFbGAEowC6lrE%3DvQmBGiPwQ2EJKcoMQUNYDwgb0d5vkjxvSPtkuqY0FFGY9D2yS8",
+          "AAAAAAAAAAAAAAAAAAAAAGfpbgEAAAAAdDlnOAJp18hBJE2XvpIFR5BT%2FlU%3Dxx6v1V8A01WYe5sol5PM7o6RF88eKMxoB5uocVXJr4U7K2Ngkr",
+          "AAAAAAAAAAAAAAAAAAAAAEgxbwEAAAAANpQeNdE%2FBiaB9RncHdfQS3a0dsI%3DfcpHpWk2XSl9zhP6tknT3PzFbWSeWiUY7LJQMM3MxhcF0g9MPf",
+          "AAAAAAAAAAAAAAAAAAAAACkXbwEAAAAA4EaRYdGe0XQmMdEz1BmhgY0W7bY%3D8TyRvFFaa8f9ti7HSiKRkwTkQMXQwNrCOZLtLVbbmEMgLVcXlb",
+          "AAAAAAAAAAAAAAAAAAAAANe%2FbAEAAAAA9jYx0Inaeh%2FbvLfz%2FdHmeN8PRXQ%3DhlJ0qUNoA5Bss5eStWnhRniTUgF2Z4AQhbcc1nrS3wEWA6Petn"
+        ]
 
 def get_time_step():
     time_step = []
-    for y in range(2020, 2023, 1):
-        for m in [1, 6]:
-            time_step.append(datetime(y, m, 1, hour=0,minute=0, tzinfo=UTC))
+    for m in [1, 3, 6, 9]:
+        time_step.append(datetime(2020, m, 1, hour=0,minute=0, tzinfo=UTC))
+    time_step.append(datetime(2021, 1, 1, hour=0,minute=0, tzinfo=UTC))
     return time_step
 
 time_step = get_time_step()
-
 
 # fake_tweet = "New Harris County #Houston #COVID19 numbers online tonight @HoustonChron. Forget the 2 week grace period you heard this AM at the House Energy Commerce testimony (which didn't make sense to me), we're at this point now, unfortunately.  Discussing tomorrow @NewDay @CNN early start https://t.co/oAqEbXbsEC"
 def hash_tags_analysis(text):
     tags = {tag.strip("#") for tag in text.split() if tag.startswith("#")}
     return tags
-    
+# print(hash_tags_analysis(fake_tweet))
+
 def at_analysis(text):
     tags = {tag.strip("@") for tag in text.split() if tag.startswith("@")}
     return tags    
-
-# print(hash_tags_analysis(fake_tweet))
 # print(at_analysis(fake_tweet))
 
 
 # text1 = '''Hilarious 😂. The feeling of making a sale 😎, The feeling of actually fulfilling orders 😒'''
-
 def sentiment_analysis(text):
     sia = SentimentIntensityAnalyzer()
     def convert_emojis(text):
@@ -65,42 +66,7 @@ def sentiment_analysis(text):
     return sia.polarity_scores(text)
 
 # print(sentiment_analysis(text1))
-
-
-# TODO task 2 working in progress
-def topic():
-    topics = pd.DataFrame({'datetime':[], 'domain':[]})
-    #topics = pd.DataFrame([[]], columns=['datetime', 'domain'])
-    id = 1243807211815649285
-    client = Client(BEARER_TOKEN)
-    tw = client.get_tweet(id, tweet_fields=["context_annotations", "created_at","entities","public_metrics"], user_fields="public_metrics")
-    # uid = tw.data.author_id
-    # print(tw)
-    # tags = {tag.strip("#") for tag in text.split() if tag.startswith("#")}
-    #print(tags)
-    #print(tw.data.context_annotations)
-    # dti = pd.date_range(start="2019-12-31", periods=6, freq="6M")
-    # dti = dti.tz_localize("UTC")
-    #print(dti) 
-    sent_date = tw.data.created_at
-    ts = get_time_step()
-    #print(sent_date)
-    # print(sent_date in range(ts[0],ts[1]))
-    # print(sent_date>ts[0])
-    # print(tw.data.entities)
-    # print(tw.data.public_metrics)
-    domains = {d["domain"]["name"] for d in tw.data.context_annotations}
-    for d in domains:
-        temp = pd.DataFrame([[sent_date, d]], columns=['datetime', 'domain'])
-        topics = pd.concat([topics, temp])
-    print(topics)
-    topics.set_index('datetime')
-    # for i in tw.data:
-    #     print(i)
-    # print(uid)
-    # print(tw.includes["users"][0].public_metrics)
     
-
 def get_time_slot(test_date):
     for i in range(0,len(time_step) - 1):
         if test_date >= time_step[i] and test_date<=time_step[i+1]:
@@ -127,34 +93,34 @@ sentiment = [dict({'neg': [], 'neu': [], 'pos': [], 'compound': []}),
 public_metrics = [dict({'followers_count': [], 'following_count': [], 'tweet_count': [], 'listed_count': []}),
                   dict({'followers_count': [], 'following_count': [], 'tweet_count': [], 'listed_count': []})]
 count = [0, 0]
-count_time = [0,0,0,0,0]
+count_time = [0,0,0,0]
 ats = [{}, {}]
-topic_time = [{},{},{},{},{}]
+topic_time = [{},{},{},{}]
+verified_count = [0,0]
 
 # run for each tweet, need add if statement depends on rumour or not
-def get_tweet(id, model, vectorizer):
-    client = Client(BEARER_TOKEN, wait_on_rate_limit=True)
+def get_tweet(id, model, vectorizer, client):
+    # client = Client(BEARER_TOKEN, wait_on_rate_limit=True)
     tweet = client.get_tweet(id, expansions = ["author_id"],tweet_fields=["context_annotations", "created_at","entities","public_metrics"], user_fields=["verified", "public_metrics"])
     if tweet.data == None:
         return
     text = tweet.data.text
     # print(text)
-    varified = tweet.includes["users"][0].verified
-    # print(varified)
+    verified = tweet.includes["users"][0].verified
+    # print(verified)
     test_text = tokenize_tweet(text)
     # print(test_text)
-    testtfidf = vectorizer.transform([test_text])
+    test_tfidf = vectorizer.transform([test_text])
     # print(testtfidf)
-    testtfidf = scipy.sparse.csr_matrix(testtfidf).todense()
+    test_tfidf = scipy.sparse.csr_matrix(test_tfidf).todense()
     # print(testtfidf)
     # test = tf.constant([[testtfidf, int(varified)]])
     # print(test)
-    testpd = pd.DataFrame(testtfidf)
-    test_verified = pd.DataFrame({"verified":[int(varified)]})
-    test = pd.concat([testpd, test_verified], axis=1)
+    test_pd = pd.DataFrame(test_tfidf)
+    test_verified = pd.DataFrame({"verified":[int(verified)]})
+    test = pd.concat([test_pd, test_verified], axis=1)
     test = tf.convert_to_tensor(test)
     # print(test)
-
     prediction = model.predict(test)
     prediction = (prediction > 0.5).astype("int32")
     prediction = np.ndarray.flatten(prediction)[0]
@@ -207,7 +173,9 @@ def get_tweet(id, model, vectorizer):
     for a in tweet_ats:
         ats[prediction][a] = ats[prediction].get(a, 0) + 1
 
-
+    if verified:
+        verified_count[prediction] +=1
+    
 def main():
     vectorizer = TfidfVectorizer()
     train_file = "train.data.txt"
@@ -217,34 +185,38 @@ def main():
     train_data.dropna(inplace=True)
     train_data = train_data.apply(lambda x: tokenize_tweet(x))
     vectorizer.fit(train_data.tolist())
-    # print(train_data)
     model = keras.models.load_model("tfidf.h5")
-    file = "covid.data.txt"
     file_open = open('./project-data/' + file, 'r')
-    counter = 0
+
+
+    clients = [Client(t, wait_on_rate_limit=True) for t in tokens]
+
+    counter = 0 
     for line in file_open.readlines():
         line = line.strip()
         ids = line.split(',')
         source_id = ids[0]
-        # id = 1243807211815649285
-        get_tweet(source_id, model, vectorizer)
+        client = clients[counter%len(clients)]
+        get_tweet(source_id, model, vectorizer, client)
         print(counter)
         counter += 1
-        # if counter > 3:
+        # if counter > 10:
         #     break
     
-    # printing out result
+    # printing out result for trend
     for i in range(0, len(topic_time)):
         print(f"Topic from {time_step[i]} to {time_step[i+1]} are \n {topic_time[i]}\n")
     
-    metrics = [topics, hashtags, sentiment, public_metrics, count, ats]
-    metric_names = ["topics", "hashtags", "sentiment", "public_metrics", "count", "ats"]
+    
+    metrics = [topics, hashtags, sentiment, public_metrics, count, ats, verified_count]
+    metric_names = ["topics", "hashtags", "sentiment", "public_metrics", "count", "ats", "verified_count"]
     for k in range(0,len(metrics)):
         for p in [0,1]:
             clf = "rumour" if p else "nonrumour"
             print(f"The {metric_names[k]} for {clf} are \n {metrics[k][p]}")
             print()
     
+    # Output topics, hashtags, and ats to files
     tasks = [topics, hashtags, ats]
     file_name = ["topics", "hashtags", "ats"]
     label = ["topic", "hashtag", "user"]
@@ -259,13 +231,14 @@ def main():
         print(task_df)
         task_df.to_csv(f"output/{file_name[num]}.csv",index=True, index_label=label[num])
         num += 1
-        
+    
+    # Output trend to files
     trend_index = []
     for d in topic_time:
         trend_index = trend_index + list(d.keys())
     trend_index = list(set(trend_index))
-    trend_dict = {"2020/1/1-2020/6/1":[], "2020/6/1-2021/1/1": [], "2021/1/1-2021/6/1": [], 
-                   "2021/6/1-2022/1/1": [], "2022/1/1-2022/6/1": []}
+    trend_dict = {"2020/1/1-2020/3/1":[], "2020/3/1-2020/6/1": [], "2020/6/1-2020/9/1": [], 
+                   "2020/9/1-2021/1/1": []}
     for i in trend_index:
         foo = 0
         for k in trend_dict.keys():
@@ -275,14 +248,14 @@ def main():
     print(trend_df)
     trend_df.to_csv("output/trend.csv",index=True, index_label="Topic")
     
-        
+    # sentiment
     sentiment_nonrumour = pd.DataFrame(data=sentiment[0])
     sentiment_rumour = pd.DataFrame(data=sentiment[1])
     print(sentiment_nonrumour)
     print(sentiment_rumour)
     sentiment_nonrumour.to_csv("output/sentiment_nonrumour.csv",index=False)
     sentiment_rumour.to_csv("output/sentiment_rumour.csv",index=False)
-
+    # metrics 
     public_metrics_nonrumour = pd.DataFrame(data=public_metrics[0])
     public_metrics_rumour = pd.DataFrame(data=public_metrics[1])
     print(public_metrics_nonrumour)
@@ -291,11 +264,12 @@ def main():
     public_metrics_rumour.to_csv("output/public_metrics_rumour.csv",index=False)
     
     count_dict = {"nonrumour": count[0], "rumour": count[1],
-                  "2020/1/1-2020/6/1":count_time[0], 
-                  "2020/6/1-2021/1/1": count_time[1], 
-                  "2021/1/1-2021/6/1": count_time[2], 
-                  "2021/6/1-2022/1/1": count_time[3], 
-                  "2022/1/1-2022/6/1": count_time[4]}
+                  "nonrumour_verified": verified_count[0], 
+                  "rumour_verified": verified_count[1],
+                  "2020/1/1-2020/3/1":count_time[0], 
+                  "2020/3/1-2020/6/1": count_time[1], 
+                  "2020/6/1-2020/9/1": count_time[2], 
+                  "2020/9/1-2021/1/1": count_time[3]}
     count_df = pd.DataFrame(data = count_dict, index=[0])
     count_df.to_csv("output/count.csv", index=True, index_label="0")
     
